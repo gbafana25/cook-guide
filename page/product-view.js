9b3cd41030c23a5f43d1c7db1cbd50741ad86890
gbafana25 <gkm25@protonmail.com>
2023-04-06 20:16:03 -0500

    styled product view

--- page/product-view.js
+++ page/product-view.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const full = new URL(window.location.toLocaleString()).searchParams;
 	let view = document.querySelector("#product").innerHTML;
+	const narray = full.get('facts').split(',');
 	let data = {
 		"name": full.get('name'),
 		"price": full.get('price'),
 		"categories": full.get('cat'),
 		"allergens": full.get('allergens'),
 		"image": full.get('image'),
+		"ingredients": full.get('ingredients'),
+		"nutri_facts": narray,
 	};
 
 	let scr = Handlebars.compile(view);
